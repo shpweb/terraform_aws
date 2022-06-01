9ca3da6bce9ce1be78b78545a8a872e8b8222a6a
Hardik Patel <hardik.patel@Hardiks-MacBook-Pro.local>
2022-06-01 20:37:53 +0530

    remove cred

--- first_ec2/first_ec2.tf
+++ first_ec2/first_ec2.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-	region = "ap-south-1"
-	access_key = "AKIAZC7OIMZENLG2YYHR"
-	secret_key = "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"
-}
+provider "aws" {}
 
 resource "aws_instance" "first_ec2_with_terraform" {
 	ami = "ami-079b5e5b3971bd10d"
