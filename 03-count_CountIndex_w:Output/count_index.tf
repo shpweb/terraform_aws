d56d74023dc171d8ef4aef8d0df7bb93ab0be0e7
Hardik Patel <hardik.patel@Hardiks-MacBook-Pro.local>
2022-06-15 11:38:40 +0530

    output with count index

---
+++ 03-count_CountIndex_w:Output/count_index.tf
@@ -0,0 +1,36 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-079b5e5b3971bd10d"
+    instance_type = "t2.micro"
+    count = 3
+    tags = {
+        Name = "myec2-${count.index}"
+    }
+}
+
+resource "aws_iam_user" "for_lb" {
+    name = var.iam_lb[count.index]
+    count = 3
+    path = "/system/"
+}
+
+output "instance_name_id" {
+    value = ["${aws_instance.myec2.*.public_ip}"]
+    description = "Public IP Address for instance"
+}
+
+output "iam_user" {
+    value = aws_iam_user.for_lb
+}
